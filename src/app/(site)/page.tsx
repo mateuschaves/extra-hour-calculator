82dd66b2d088784e0d4b0a57d0e16b5ff59c1e91
Mateus Henrique <mateushenriquechaves1@gmail.com>
2023-10-26 15:21:09 -0300

    feat: styling my extra hours container

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,10 +1,44 @@
-import { Container, Heading } from '@chakra-ui/react'
+'use client'
+
+import { Box, Button, Container, Divider, Flex, Heading, Text } from '@chakra-ui/react'
+import { AddIcon } from '@chakra-ui/icons'
 import React from 'react'
 
 export default function Home() {
   return (
-    <Container maxWidth={'container.xl'} backgroundColor={'Background'} borderRadius={'md'} padding={'10'} marginY={'20'}>
-      <Heading fontWeight={'medium'} size={'md'}>Calculadora de Horas Extras</Heading>
+    <Container
+      maxWidth={'container.md'}
+      backgroundColor={'Background'}
+      borderRadius={'md'}
+      paddingX={'4'}
+      paddingY={4}
+      marginY={'20'}
+      borderTopColor={'blackAlpha.900'}
+      borderTopWidth={'6px'}
+    >
+      <Heading
+        fontWeight={'bold'}
+        size={'lg'}
+      >
+        Minhas horas
+      </Heading>
+      <Divider marginY={4} />
+      <Text align={'center'}>
+        Sem horas extras 🥲
+      </Text>
+      <Divider marginY={4} />
+      <Flex flex={1} width={'inherit'} flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'}>
+        <Flex>
+          <Text colorScheme='gray'>R$ 9.000,00</Text>
+        </Flex>
+        <Button 
+          leftIcon={<AddIcon />} 
+          type='button' 
+          variant={'ghost'}
+          >
+            Adicionar horas
+        </Button>
+      </Flex>
     </Container>
   )
 }
